98cd0c58598d43fea2439ba652c5cff1ee03cf08
CastelJeremy <39135270+CastelJeremy@users.noreply.github.com>
2023-02-24 00:37:45 +0100

    init: project first draft

---
+++ README.md
@@ -0,0 +1,16 @@
+# todo.sh
+
+Basic todo list made with bash.
+
+![Short Demo](./demo.gif)
+
+## Usage
+
+```
+todo.sh                                 List todos
+todo.sh -a "Read the f*cking manual"    Add a todo
+todo.sh -u 1                            Toggle todo #1 state
+todo.sh -s 2 3                          Switch todo #2 with todo #3
+todo.sh -rm 1                           Remove todo #1
+```
+
---
+++ todo.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+DATA_FILE=/tmp/todo.txt
+
+STRIKE="\e[9m"
+RESET="\e[0m"
+COLOR="\e[49;38;5;81m"
+
+parse_task() {
+	done=`echo $2 | cut -c 1`
+	str=`echo $2 | cut -c 2-`
+	if [ "$done" = "1" ]; then
+		str="${STRIKE}${str}${RESET}"
+	fi
+
+	if [ $1 -lt 10 ]; then
+		echo -e "${COLOR}0${i}${RESET} $str"
+	else
+		echo -e "${COLOR}$i${RESET} $str"
+	fi
+}
+
+list_tasks() {
+	i=0;
+	cat $DATA_FILE | while read line; do
+		i=$((i+1))
+        parse_task $i "$line"
+	done
+}
+
+show_error() {
+    echo "todo.sh: $1"
+    echo "Try 'todo.sh --help' for more information."
+}
+
+if [ ! -e $DATA_FILE ]; then
+    echo "todo.sh: cannot stat '$DATA_FILE': No such file"
+    exit
+fi
+
+if [ ! -w $DATA_FILE ]; then
+    echo "todo.sh: cannot stat '$DATA_FILE': Permission denied"
+    exit
+fi
+
+SIZE=`<$DATA_FILE wc -l`
+
+if [ "$1" = "a" ] || [ "$1" = "-a" ] || [ "$1" = "--add" ]; then
+    if [ $# -lt 2 ]; then
+        show_error "missing content operand after '$1'"
+        exit
+    fi
+	echo "0$2" >> $DATA_FILE
+elif [ "$1" = "r" ] || [ "$1" = "-rm" ] || [ "$1" = "--remove" ]; then
+    if [ $# -lt 2 ]; then
+        show_error "missing number operand after '$1'"
+        exit
+    elif [ $2 -lt 1 ] || [ $2 -gt $SIZE ]; then
+        show_error "invalid number operand '$2'"
+        exit
+    fi
+
+	sed -i "$2d" $DATA_FILE
+elif [ "$1" = "u" ] || [ "$1" = "-u" ] || [ "$1" = "--update" ]; then
+    if [ $# -lt 2 ]; then
+        show_error "missing number operand after '$1'"
+        exit
+    elif [ $2 -lt 1 ] || [ $2 -gt $SIZE ]; then
+        show_error "invalid number operand '$2'"
+        exit
+    fi
+
+    done=`sed "$2!d" $DATA_FILE | cut -c 1`
+
+    if [ "$done" = "0" ]; then
+        sed -i "$2s/0\(.*\)/1\1/" $DATA_FILE
+    elif [ "$done" = "1" ]; then
+        sed -i "$2s/1\(.*\)/0\1/" $DATA_FILE
+    fi
+elif [ "$1" = "s" ] || [ "$1" = "-s" ] || [ "$1" = "--switch" ]; then
+    if [ $# -lt 3 ]; then
+        show_error "missing number operand after '${@: -1}'"
+        exit
+    elif [ $2 -lt 1 ] || [ $2 -gt $SIZE ]; then
+        show_error "invalid number operand '$2'"
+        exit
+    elif [ $3 -lt 1 ] || [ $3 -gt $SIZE ]; then
+        show_error "invalid number operand '$3'"
+        exit
+    fi
+
+    a=`sed "$2!d" $DATA_FILE`
+    b=`sed "$3!d" $DATA_FILE`
+    sed -i "$3s/.*/$a/" $DATA_FILE
+    sed -i "$2s/.*/$b/" $DATA_FILE
+elif [ "$1" = "h" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Help section todo"
+    exit
+fi
+
+list_tasks
+
